20dc2f587d694dc62a1cdc32f0a050401c0fb4aa
JohannLee1996 <johannlee@qq.com>
2019-05-29 14:09:47 -0300

    f1score

--- run_classifier.py
+++ run_classifier.py
@@ -781,12 +781,22 @@ def model_fn_builder(bert_config, num_labels, init_checkpoint, learning_rate,
 
       def metric_fn(per_example_loss, label_ids, logits, is_real_example):
         predictions = tf.argmax(logits, axis=-1, output_type=tf.int32)
+        recall = tf.metrics.recall(
+          labels=label_ids, predictions=predictions, weights=is_real_example
+        )
+        precision = tf.metrics.precision(
+          labels=label_ids, predictions=predictions, weights=is_real_example
+        )
+        f1score = 2*recall*precision/(recall+precision)
         accuracy = tf.metrics.accuracy(
             labels=label_ids, predictions=predictions, weights=is_real_example)
         loss = tf.metrics.mean(values=per_example_loss, weights=is_real_example)
         return {
             "eval_accuracy": accuracy,
             "eval_loss": loss,
+            "precision": precision,
+            "recall": recall,
+            "f1score": f1score,
         }
 
       eval_metrics = (metric_fn,
